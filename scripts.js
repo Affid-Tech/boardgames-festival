ad17e81807fb1e9548ee5dc6b3a228a6f9f06960
Affid <47119594+Affid@users.noreply.github.com>
2025-04-24 17:55:51 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ fetch('sources/tournaments.json')
     }
     cards.push(`
         <div class="col-12 col-md-12 col-lg-12 text-center">
-        <a href="https://t.me/Affid_fedorov" class="btn btn-primary btn-lg px-5 mt-3">Хочу провести игру!</a>
+        <a href="https://t.me/Affid_fedorov" class="btn btn-primary btn-lg px-5 mt-3 mx-3">Хочу провести игру!</a>
           <a href="https://forms.gle/Qhr4FQAvPULJRdo99" target="_blank" class="btn btn-outline-primary btn-lg mt-3">
             Выбери игры для дня настольных игр!
           </a>
